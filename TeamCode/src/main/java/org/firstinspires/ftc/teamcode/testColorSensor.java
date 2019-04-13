4704e1e04d05aad1137a3b11c60a89a34a9245ff
prestonraab <prestonraab@gmail.com>
2019-04-13 11:59:12 -0700

    Not needed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testColorSensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testColorSensor.java
@@ -2,21 +2,13 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.Gamepad;
 
 @TeleOp(name="testColorSensor", group="Testing")
 public class testColorSensor extends LinearOpMode
 {
-    Bogg robot;
-
-    Gamepad g1;
-
     @Override
     public void runOpMode()
     {
-        robot = Bogg.determineRobot(hardwareMap, telemetry);
-        robot.driveEngine.driveAtAngle(Math.PI);
-        g1 = gamepad1;
         MyColorSensor myColorSensor = new MyColorSensor(hardwareMap);
         waitForStart();
 
@@ -26,7 +18,7 @@ public class testColorSensor extends LinearOpMode
             telemetry.addData("color", myColorSensor.getColor().name());
             telemetry.addData("typicalHue", myColorSensor.getColor().typicalHue);
 
-            robot.update();
+            telemetry.update();
             idle();
         }
     }
